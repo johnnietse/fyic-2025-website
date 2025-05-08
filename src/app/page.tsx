f874bb3b2ce1a70886f34e2cd4b5b3481b11e7d8
Johnnie Tse <johnnietse994@gmail.com>
2025-05-08 02:32:33 -0400

    new-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Portfolio() {
       {/* <EventContent /> */}
       <Faq />
       <main className="w-full max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8 py-8 md:py-12 lg:py-16 bg-white">
-        <Contact />
+      {/* <Contact /> */}
       </main>
       <Footer />
     </>
