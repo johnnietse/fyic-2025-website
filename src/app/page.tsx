92d1efa2f7fa9a4895a2722b2f693f1350658044
Johnnie Tse <138593553+johnnietse@users.noreply.github.com>
2025-07-12 20:42:16 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Portfolio() {
       </AnimatedSection>
 
       <main className="w-full max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8 py-8 md:py-12 lg:py-16 bg-white">
-      {/* <Contact /> */}
+        <Contact /> 
       </main>
 
       {/* </Animation> */}
