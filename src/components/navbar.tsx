a6877c49ff2412f9e495f94a9c98e3dc87e124d4
Johnnie Tse <138593553+johnnietse@users.noreply.github.com>
2025-08-29 06:07:38 -0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,9 +20,10 @@ import {
   UsersIcon,
   HandRaisedIcon,
   BriefcaseIcon
-
 } from "@heroicons/react/24/solid";
 
+// Import your logo (adjust the path as needed)
+import logo from "../fyic-logo.png";
 
 interface NavItemProps {
   children: React.ReactNode;
@@ -35,7 +36,7 @@ function NavItem({ children, href }: NavItemProps) {
       <Typography
         as="a"
         href={href || "#"}
-        target={href = "_self"}
+        target={href ? "_blank" : "_self"}
         variant="paragraph"
         className="flex items-center gap-2 font-medium"
         style={{ fontFamily: 'Montserrat, sans-serif' }}
@@ -48,19 +49,15 @@ function NavItem({ children, href }: NavItemProps) {
 }
 
 const NAV_MENU = [
-
-  
   {
     name: "Home",
     icon: HomeIcon,
     href: "/",
-    
   },
   {
     name: "Speakers",
     icon: UserCircleIcon,
     href: "/speakers",
-
   },
   {
     name: "Sponsorship",
@@ -117,14 +114,21 @@ export function Navbar() {
       {...({} as any)}
     >
       <div className="container mx-auto flex items-center justify-between">
-        <Typography
-          color={isScrolling ? "blue-gray" : "white"}
-          className="text-lg font-bold"
-          style={{ fontFamily: 'Montserrat, sans-serif' }}
-          {...({} as any)}
-        >
-          FYIC 2025
-        </Typography>
+        <div className="flex items-center">
+          <img 
+            src={logo} 
+            alt="FYIC Logo" 
+            className="h-8 w-8 mr-2" // Adjust size and spacing as needed
+          />
+          <Typography
+            color={isScrolling ? "blue-gray" : "white"}
+            className="text-lg font-bold"
+            style={{ fontFamily: 'Montserrat, sans-serif' }}
+            {...({} as any)}
+          >
+            FYIC 2025
+          </Typography>
+        </div>
         <ul
           className={`ml-10 hidden items-center gap-6 lg:flex ${
             isScrolling ? "text-gray-900" : "text-white"
@@ -137,15 +141,6 @@ export function Navbar() {
             </NavItem>
           ))}
         </ul>
-
-        {/* <div className="hidden items-center gap-4 lg:flex">
-          <Button color={isScrolling ? "gray" : "white"} variant="text">
-            Log in
-          </Button>
-          <a href="https://www.material-tailwind.com/blocks" target="_blank">
-            <Button color={isScrolling ? "gray" : "white"}>blocks</Button>
-          </a>
-        </div> */}
 
         <IconButton
           variant="text"
@@ -172,14 +167,6 @@ export function Navbar() {
               </NavItem>
             ))}
           </ul>
-          
-          {/* <div className="mt-6 flex items-center gap-4">
-            <Button variant="text">Log in</Button>
-            <a href="https://www.materila-tailwind.com/blocks" target="_blank">
-              <Button color="gray">blocks</Button>
-            </a>
-          </div> */}
-
         </div>
       </Collapse>
     </MTNavbar>
@@ -187,3 +174,206 @@ export function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import logo from "../fyic-logo.png";
+// import {
+//   Navbar as MTNavbar,
+//   Collapse,
+//   Button,
+//   IconButton,
+//   Typography
+// } from "@material-tailwind/react";
+// import {
+//   RectangleStackIcon,
+//   UserCircleIcon,
+//   CommandLineIcon,
+//   Squares2X2Icon,
+//   XMarkIcon,
+//   Bars3Icon,
+//   HomeIcon,
+//   MicrophoneIcon,
+//   CurrencyDollarIcon,
+//   CalendarDaysIcon,
+//   UsersIcon,
+//   HandRaisedIcon,
+//   BriefcaseIcon
+
+// } from "@heroicons/react/24/solid";
+
+
+// interface NavItemProps {
+//   children: React.ReactNode;
+//   href?: string;
+// }
+
+// function NavItem({ children, href }: NavItemProps) {
+//   return (
+//     <li>
+//       <Typography
+//         as="a"
+//         href={href || "#"}
+//         target={href = "_self"}
+//         variant="paragraph"
+//         className="flex items-center gap-2 font-medium"
+//         style={{ fontFamily: 'Montserrat, sans-serif' }}
+//         {...({} as any)}
+//       >
+//         {children}
+//       </Typography>
+//     </li>
+//   );
+// }
+
+// const NAV_MENU = [
+
+  
+//   {
+//     name: "Home",
+//     icon: HomeIcon,
+//     href: "/",
+    
+//   },
+//   {
+//     name: "Speakers",
+//     icon: UserCircleIcon,
+//     href: "/speakers",
+
+//   },
+//   {
+//     name: "Sponsorship",
+//     icon: BriefcaseIcon,
+//     href: "/sponsors",
+//   },
+//   {
+//     name: "Itinerary",
+//     icon: CalendarDaysIcon,
+//     href: "/agenda",
+//   },
+//   {
+//     name: "Our Team",
+//     icon: UsersIcon,
+//     href: "/team",
+//   },
+// ];
+
+// export function Navbar() {
+//   const [open, setOpen] = React.useState(false);
+//   const [isScrolling, setIsScrolling] = React.useState(false);
+
+//   const handleOpen = () => setOpen((cur) => !cur);
+
+//   React.useEffect(() => {
+//     window.addEventListener(
+//       "resize",
+//       () => window.innerWidth >= 960 && setOpen(false)
+//     );
+//   }, []);
+
+//   React.useEffect(() => {
+//     function handleScroll() {
+//       if (window.scrollY > 0) {
+//         setIsScrolling(true);
+//       } else {
+//         setIsScrolling(false);
+//       }
+//     }
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
+
+//   return (
+//     <MTNavbar
+//       shadow={false}
+//       fullWidth
+//       blurred={false}
+//       color={isScrolling ? "white" : "transparent"}
+//       className="fixed top-0 z-50 border-0"
+//       style={{ fontFamily: 'Montserrat, sans-serif' }}
+//       {...({} as any)}
+//     >
+//       <div className="container mx-auto flex items-center justify-between">
+//         <Typography
+//           color={isScrolling ? "blue-gray" : "white"}
+//           className="text-lg font-bold"
+//           style={{ fontFamily: 'Montserrat, sans-serif' }}
+//           {...({} as any)}
+//         >
+//           FYIC 2025
+//         </Typography>
+//         <ul
+//           className={`ml-10 hidden items-center gap-6 lg:flex ${
+//             isScrolling ? "text-gray-900" : "text-white"
+//           }`}
+//         >
+//           {NAV_MENU.map(({ name, icon: Icon, href }) => (
+//             <NavItem key={name} href={href}>
+//               <Icon className="h-5 w-5" style={{ fontFamily: 'Montserrat, sans-serif' }}/>
+//               <span>{name}</span>
+//             </NavItem>
+//           ))}
+//         </ul>
+
+//         {/* <div className="hidden items-center gap-4 lg:flex">
+//           <Button color={isScrolling ? "gray" : "white"} variant="text">
+//             Log in
+//           </Button>
+//           <a href="https://www.material-tailwind.com/blocks" target="_blank">
+//             <Button color={isScrolling ? "gray" : "white"}>blocks</Button>
+//           </a>
+//         </div> */}
+
+//         <IconButton
+//           variant="text"
+//           color={isScrolling ? "gray" : "white"}
+//           onClick={handleOpen}
+//           className="ml-auto inline-block lg:hidden"
+//           style={{ fontFamily: 'Montserrat, sans-serif' }}
+//           {...({} as any)}
+//         >
+//           {open ? (
+//             <XMarkIcon strokeWidth={2} className="h-6 w-6" />
+//           ) : (
+//             <Bars3Icon strokeWidth={2} className="h-6 w-6" />
+//           )}
+//         </IconButton>
+//       </div>
+//       <Collapse open={open}>
+//         <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+//           <ul className="flex flex-col gap-4 text-gray-900" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+//             {NAV_MENU.map(({ name, icon: Icon, href }) => (
+//               <NavItem key={name} href={href}>
+//                 <Icon className="h-5 w-5" style={{ fontFamily: 'Montserrat, sans-serif' }}/>
+//                 {name}
+//               </NavItem>
+//             ))}
+//           </ul>
+          
+//           {/* <div className="mt-6 flex items-center gap-4">
+//             <Button variant="text">Log in</Button>
+//             <a href="https://www.materila-tailwind.com/blocks" target="_blank">
+//               <Button color="gray">blocks</Button>
+//             </a>
+//           </div> */}
+
+//         </div>
+//       </Collapse>
+//     </MTNavbar>
+//   );
+// }
+
+// export default Navbar;
