340452f84bcc19c11b523e36498e0a1cef03d376
johnnietse <johnnietse994@gmail.com>
2025-09-18 22:06:10 -0400

    chore: migrate from next-on-pages to opennext

---
+++ opennext.config.js
@@ -0,0 +1,7 @@
+// opennext.config.js
+/** @type {import('@opennextjs/cloudflare').Config} */
+const config = {
+    // Optional: Add any OpenNext specific configuration here
+  };
+  
+  export default config;
